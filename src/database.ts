460f8ea8539e9b03ad97ee4538c2324b1ebe55e6
Ken Ovienadu <kenovienadu@gmail.com>
2025-02-27 09:14:23 +0100

    feat: update dotenv import and enhance MongoDB connection methods

--- src/database.ts
+++ src/database.ts
@@ -6,19 +6,27 @@ class Database {
     try {
       await mongoose.connect(config.MONGODB_URL, );
       console.log("Connected to MongoDB");
+      return true;
     } catch (error) {
       console.error("Error connecting to MongoDB", error);
+      return false;
     }
   }
 
-    public static async disconnect() {
-        try {
-        await mongoose.disconnect();
-        console.log("Disconnected from MongoDB");
-        } catch (error) {
-        console.error("Error disconnecting from MongoDB", error);
-        }
+  public static async disconnect() {
+    try {
+      await mongoose.disconnect();
+      console.log("Disconnected from MongoDB");
+      return true;
+    } catch (error) {
+      console.error("Error disconnecting from MongoDB", error);
+      return false;
     }
+  }
+
+  public static getInstance() {
+    return mongoose;
+  }
 }
 
 export default Database;
