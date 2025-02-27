08ecad804af89e86f2421ce8823b7cddfb37d97c
Ken Ovienadu <kenovienadu@gmail.com>
2025-02-27 09:14:56 +0100

    feat: add ESLint configuration for TypeScript support

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.stylisticTypeChecked,
+    tseslint.configs.recommendedTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                // @ts-ignore
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+);
