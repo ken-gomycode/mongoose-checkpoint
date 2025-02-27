16a10a003440df5ccefbc57fea0b01a4c8c4f751
Ken Ovienadu <kenovienadu@gmail.com>
2025-02-27 09:14:48 +0100

    feat: enhance application entry point and update TypeScript configuration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-// App entry point
+import Database from "./database";
+import createPerson from "./handlers/createPerson";
+import createPeople from "./handlers/createPeople";
+import {findPeople, findPerson} from "./handlers/findPeople";
+import updatePerson from "./handlers/updatePerson";
+import removePeople from "./handlers/removePeople";
+
+
+const runHandlers = async () => {
+  const connected = await Database.connect()
+  if (!connected) {
+    console.error('Failed to connect to MongoDB');
+    return;
+  }
+
+  await createPerson();
+  await createPeople();
+  await findPeople();
+  await findPerson();
+  await updatePerson();
+  await removePeople()
+
+  await Database.disconnect();
+}
+
+runHandlers().catch(error => {
+  console.error('Error running handlers', error);
+  process.exit(0);
+})
